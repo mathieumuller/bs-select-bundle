44f1d17c44b153a9b2173bf2810f4cd228c7da24
mathieumuller <mathieu.1984@live.fr>
2016-05-20 12:35:24 +0200

    Update AxiolabBootstrapSelectExtension.php

--- DependencyInjection/AxiolabBootstrapSelectExtension.php
+++ DependencyInjection/AxiolabBootstrapSelectExtension.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Axiolab\BootstrapSelectBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -12,24 +13,7 @@ class AxiolabBootstrapSelectExtension extends Extension implements PrependExtens
     public function prepend(ContainerBuilder $container)
     {
         $configs = $container->getExtensionConfig($this->getAlias());
-        $config  = $this->processConfiguration(new Configuration(), $configs);
-
-        $bundles = $container->getParameter('kernel.bundles');
-        //prepend assetic
-        if (true === isset($bundles['AsseticBundle'])) {
-            $this->configureAssetic($container);
-        }
-        //prepend twig
-        if (true === isset($bundles['TwigBundle'])) {
-            $this->configureTwig($container, $config);
-        }
-    }
-
-
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config = $this->processConfiguration(new Configuration(), $configs);
 
         $loader = new YamlFileLoader(
             $container,
@@ -43,6 +27,20 @@ class AxiolabBootstrapSelectExtension extends Extension implements PrependExtens
             $bundleParameters[$node] = $value;
         }
         $container->setParameter('axiolab_bootstrap_select', $bundleParameters);
+
+        $bundles = $container->getParameter('kernel.bundles');
+        //prepend assetic
+        if (true === isset($bundles['AsseticBundle'])) {
+            $this->configureAssetic($container);
+        }
+        //prepend twig
+        if (true === isset($bundles['TwigBundle'])) {
+            $this->configureTwig($container, $config);
+        }
+    }
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
     }
 
     public function getAlias()
@@ -52,16 +50,17 @@ class AxiolabBootstrapSelectExtension extends Extension implements PrependExtens
 
     protected function configureAssetic(ContainerBuilder $container)
     {
+        $language = $container->getParameter('axiolab_bootstrap_select')['preferred_language'];
         $jsPath = '%kernel.root_dir%/../vendor/axiolab/bs-select-bundle/Resources/public/js';
         $jsConfig = [
             'axiolab_bootstrap_select' => [
-                'inputs'  => [
+                'inputs' => [
                     $jsPath.'/bootstrap-select.js',
-                    $jsPath.'/i18n/defaults-fr_FR.min.js',
+                    $jsPath.'/i18n/defaults-'.$language.'.min.js',
                     $jsPath.'/ajax.js',
                 ],
-                'output' => 'js/axiolabbootstrapselect.js'
-            ]
+                'output' => 'js/axiolabbootstrapselect.js',
+            ],
         ];
 
         foreach ($container->getExtensions() as $name => $extension) {
@@ -86,7 +85,7 @@ class AxiolabBootstrapSelectExtension extends Extension implements PrependExtens
                     $container->prependExtensionConfig(
                         $name,
                         [
-                            'form_themes' => [$config['form_resource']]
+                            'form_themes' => [$config['form_resource']],
                         ]
                     );
                     break;
